d7cc3ef82a32d48642a7c6b68dee2e6afeee1b51
Sevada797 <64130935+Sevada797@users.noreply.github.com>
2023-09-20 06:28:56 +0400

    Update send.php

--- send.php
+++ send.php
@@ -4,8 +4,8 @@ $msg = $_GET["msg"];
 if (strlen($msg) > 400) {die("<script>alert('The max limit of message should be 400 letters.'); window.location = 'index.php' </script>");}
 if ($msg == NULL) {die("<script>alert('Please fill up message-box.'); window.location = 'index.php'</script>");}
 header('location: index.php');
-echo $msg;
-echo urldecode($msg);
+//echo $msg;
+//echo urldecode($msg);
 $mfile = fopen("chat.txt", "a") or die("Sorry somthing is not working"); 
 fwrite($mfile, ($_SERVER["REMOTE_ADDR"] . ":" . $_COOKIE["usr"] . ": " . $msg . "\n"));
 fclose($mfile);
